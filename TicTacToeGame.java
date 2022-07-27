5b796fa8498c010d2f32f6e804471a8a4449428f
mehulbhange <bhangemehul@gmail.com>
2022-07-27 08:38:02 +0530

    [Mehul]:added char array in TicTacToeGame

---
+++ TicTacToeGame.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.tictactoe;
+
+public class TicTacToeGame {
+
+    static char[] array = new char[10];
+
+    public static void main(String[] args) {
+        System.out.println("Tic tac toe");
+
+    }
+}
