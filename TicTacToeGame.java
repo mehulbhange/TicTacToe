6eae83af33ad43bdd0463692396a7da076f3427b
mehulbhange <bhangemehul@gmail.com>
2022-07-27 09:02:35 +0530

    [Mehul]:added TicTacToeGame class

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,7 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
-
-    static char[] board = new char[10];
-
+    
     public static void main(String[] args) {
         System.out.println("Tic tac toe");
 
