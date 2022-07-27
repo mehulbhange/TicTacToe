a1ab7e7e0f9e65b2756a692cd21e9e8395377920
mehulbhange <bhangemehul@gmail.com>
2022-07-27 09:05:10 +0530

    [Mehul]:added UC1

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
-    
+    static char[] board = new char[10];
+
     public static void main(String[] args) {
         System.out.println("Tic tac toe");
+        for(int i=0;i<10;i++){
+            board[i] = ' ';
+        }
 
     }
 }
